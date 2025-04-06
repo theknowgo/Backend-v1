35e64ba36e0baaf963d7da9b31ac99e9174b94a0
thepogostar <the.knowgo.localmate@gmail.com>
2025-04-06 15:29:01 +0530

    Update otp.service.js

--- services/otp.service.js
+++ services/otp.service.js
@@ -18,8 +18,8 @@ export const verifyOTP = async (phoneNumber, otp) => {
       return false;
     }
     if (storedOTP === otp) {
-      // await client.del(`loginotp:${phoneNumber}`);
-      otpStore.delete(phoneNumber); // Remove OTP after verification
+      await client.del(`loginotp:${phoneNumber}`);
+      // otpStore.delete(phoneNumber); // Remove OTP after verification
       console.log(`✅ OTP verified for ${phoneNumber}`);
       return true;
     } else {
